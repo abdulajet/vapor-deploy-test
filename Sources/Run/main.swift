1b362aca4f2464062f4db778d6e74bf84ce975cc
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2020-11-06 13:42:08 +0000

    remove urlsession

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -6,14 +6,14 @@ let client = Vonage(apiKey: Environment.get("APIKEY")!, apiSecret: Environment.g
 
 defer { app.shutdown() }
 
-app.post("request") { req -> EventLoopFuture<String> in
+app.post("request") { req -> EventLoopFuture<ClientResponse> in
     let body = try req.content.decode(Vonage.RequestVerificationBody.self)
-    return client.requestVerification(with: body, eventLoop: req.eventLoop)
+    return client.requestVerification(with: body, client: req.client)
 }
 
-app.post("check") { req -> EventLoopFuture<String> in
+app.post("check") { req -> EventLoopFuture<ClientResponse> in
     let body = try req.content.decode(Vonage.CheckVerificationBody.self)
-    return client.checkVerification(with: body, eventLoop: req.eventLoop)
+    return client.checkVerification(with: body, client: req.client)
 }
 
 try app.run()
--- Sources/SwiftVerify/Vonage.swift
+++ Sources/SwiftVerify/Vonage.swift
@@ -1,54 +1,54 @@
-import NIO
-import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
+import NIO
+import Vapor
+import Foundation
 
 public struct Vonage {
     private let apiKey: String
     private let apiSecret: String
-    private let apiHandler = APIHandler()
     
     public init(apiKey: String, apiSecret: String) {
         self.apiKey = apiKey
         self.apiSecret = apiSecret
     }
     
-    public func requestVerification(with body: RequestVerificationBody, eventLoop: EventLoop) -> EventLoopFuture<String> {
-        return apiHandler.sendRequest(
-            host: "api.nexmo.com",
-            path: "/verify/json",
-            body: RequestVerificationBody(body: body, apiKey: apiKey, apiSecret: apiSecret),
-            eventLoop: eventLoop
-        )
+    public func requestVerification(with body: RequestVerificationBody, client: Client) -> EventLoopFuture<ClientResponse> {
+        return client.post(URI(scheme: "https", host: "api.nexmo.com", path: "/verify/json")) { req in
+            try req.content.encode(RequestVerificationBody(body: body, apiKey: apiKey, apiSecret: apiSecret), as: .json)
+        }
     }
     
-    public func checkVerification(with body: CheckVerificationBody, eventLoop: EventLoop) -> EventLoopFuture<String> {
-        return apiHandler.sendRequest(
-            host: "api.nexmo.com",
-            path: "/verify/check/json",
-            body: CheckVerificationBody(body: body, apiKey: apiKey, apiSecret: apiSecret),
-            eventLoop: eventLoop
-        )
+    public func checkVerification(with body: CheckVerificationBody, client: Client) -> EventLoopFuture<ClientResponse> {
+        return client.post(URI(scheme: "https", host: "api.nexmo.com", path: "/verify/check/json")) { req in
+            try req.content.encode(CheckVerificationBody(body: body, apiKey: apiKey, apiSecret: apiSecret), as: .json)
+        }
     }
     
-    public struct RequestVerificationBody: Codable {
+    public struct RequestVerificationBody: Content {
         let number: String
-        let brand: String?
-        let workflowID: Int?
+        let brand: String = "SwiftVerify"
+        let workflowID: Int = 6
         var apiKey: String?
         var apiSecret: String?
         
         init(body: RequestVerificationBody, apiKey: String, apiSecret: String) {
             self.number = body.number
-            self.brand = "SwiftVerify"
-            self.workflowID = 6
             self.apiKey = apiKey
             self.apiSecret = apiSecret
         }
+        
+        private enum CodingKeys: String, CodingKey {
+            case number
+            case brand
+            case workflowID = "workflow_id"
+            case apiKey = "api_key"
+            case apiSecret = "api_secret"
+        }
     }
     
-    public struct CheckVerificationBody: Codable {
+    public struct CheckVerificationBody: Content {
         let requestID: String
         let code: String
         var apiKey: String?
@@ -59,6 +59,13 @@ public struct Vonage {
             self.code = body.code
             self.apiKey = apiKey
             self.apiSecret = apiSecret
+        }
+        
+        private enum CodingKeys: String, CodingKey {
+            case requestID = "request_id"
+            case code
+            case apiKey = "api_key"
+            case apiSecret = "api_secret"
         }
     }
 }
